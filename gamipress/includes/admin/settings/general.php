fc60b825aa39c0b19c77bd7cab41dfd1bae514e1
Sean Murphy <sean@iamseanmurphy.com>
2025-05-15 14:17:30 +0000

    7.4.2

--- gamipress/includes/admin/settings/general.php
+++ gamipress/includes/admin/settings/general.php
@@ -153,9 +153,9 @@ function gamipress_get_allowed_manager_capabilities() {
         );
     } else {
         $allowed_capabilities = array(
-            'manage_options' => 'Administrator',
-            'delete_others_posts' => 'Editor',
-            'publish_posts' => 'Author', 
+            'manage_options' => 'manage_options',
+            'delete_others_posts' => 'delete_others_posts',
+            'publish_posts' => 'publish_posts', 
         );
     }
     
